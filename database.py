210ee938cc8088e69b6f220ae4126b091f477b2c
user <user@penguin>
2025-02-24 21:26:02 -0300

    update database

--- database.py
+++ database.py
@@ -1,22 +1,33 @@
+from flask import g, current_app
+import click
 import sqlite3
 import os
 
-# TODO: change it to an env var.
-#TODO: Test for a server
-DATABASE = "database.db"
+sqlite3.register_converter("timestamp",
+                           lambda x : datetime.fromisoformat(x.decode()))
 
-def get_connection(database = DATABASE):
-    con = sqlite3.connect(database, check_same_thread=False)
-    con.row_factory = sqlite3.Row
-    return con, con.cursor()
+def get_db():
+    if "db" not in g:
+        g.db = sqlite3.connect(current_app.config["DATABASE"],
+                               detect_types = sqlite3.PARSE_DECLTYPES)
+        g.db.row_factory = sqlite3.Row
+    return g.db
 
-def init_database():
-    con, cur = get_connection()
-    for f in [f for f in os.listdir() if f.endswith(".sql")]:
-        with open(f, "r") as x:
-            s=x.read()
-        try: con.executescript(s)
-        except sqlite3.Error as e:
-            print(f"error when reading {f}: {e}")
-        con.commit()
-        con.close()
+def close_db(_):
+    db = g.pop("db", None)
+    if db: db.close()
+
+def init_db():
+    db = get_db()
+    for fn in [f for f in os.listdir("sql") if f.endswith(".sql")]:
+        with current_app.open_resource(os.path.join("sql", fn)) as f:
+            db.executescript(f.read().decode("utf8"))
+
+@click.command("init-db")
+def init_db_command():
+    init_db()
+    click.echo("the database was initialized")
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
