9f517ff4e54475a815249c98b33fbc5dc1f250d3
user <user@penguin>
2025-03-01 17:01:01 -0300

    add: register test

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -4,20 +4,22 @@ from flask import session
 
 from apptalk.database import get_db
 
-def test_register(client, app):
+@pytest.mark.parametrize(
+    ("name", "email", "username", "password"),
+    (("Alice", "alice@example.com", "Alice123", "SuperSecretAlicePassword"),
+     ("Bob", "Bob@example.com", "Bob456", "SuperSecretBobPassword"))
+)
+
+def test_register(client, auth, app, name, email, username, password):
     assert client.get("/cadastro").status_code == 200
 
-    response = client.post("/cadastro", data={
-        "name": "a", "email": "a", "username": "a", "password": "a"
-    })
+    response = auth.register(name, email, username, password)
     assert response.headers["Location"] == "/login"
 
     with app.app_context():
-        assert (get_db() \
-                .execute("SELECT * FROM users WHERE username = 'a'") \
-                .fetchone()
-                is not None)
-
+        assert (get_db()
+                .execute(f"SELECT * FROM users WHERE username = '{username}'")
+                .fetchone())
 
 def test_login(client, auth):
     assert client.get("/login").status_code == 200
